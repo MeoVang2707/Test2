f4c26b313fcf699d4c7249ef440c2983de20a4ba
MeoVang2707 <Vuhuyhoang513@gmail.com>
2016-11-05 22:15:41 +0700

    test-45

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ def home_page2():
             return render_template("search.html", search_list=Search, search_key=search_key_0, name = session["user"])
     else:
         if request.method == "GET":
-            return render_template("test_home_page2.html", user_list = Person.objects)
+            return render_template("home_page2.html", user_list = Person.objects)
         if request.method == "POST":
             search_key_0 = request.form["search"]
             search_key = search_key_0.upper()
